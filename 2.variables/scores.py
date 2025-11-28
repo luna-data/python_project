ff4a4664813e37b45b5c52c5a8470734f3f3a21e
luna-data <hjl7891@gmail.com>
2025-11-28 02:57:15 +0000

    2eksdnjs

---
+++ 2.variables/scores.py
@@ -0,0 +1,12 @@
+scores = []
+print("학생 3명의 성적을 입력하세요: ")
+score = int(input("1번 학생의 성적을 입력하세요: "))
+scores.append(score)
+score = int(input("2번 학생의 성적을 입력하세요: "))
+scores.append(score)
+score = int(input("3번 학생의 성적을 입력하세요: "))
+scores.append(score)
+max_score = max(scores)
+min_score = min(scores)
+print(f"최댓값: {max_score}")
+print(f"최솟값: {min_score}")
