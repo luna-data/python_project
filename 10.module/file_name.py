6b7ec778189990fe8efcf769251a9661e52a5588
luna-data <hjl7891@gmail.com>
2025-11-28 13:11:46 +0000

    커밋

--- 10.module/file_name.py
+++ 10.module/file_name.py
@@ -3,3 +3,16 @@ import os
 def change_extension(directory, old_ext, new_ext):
     if not os.path.isdir(directory):
         print(f"'{directory}' 는 유효한 디렉토리가 아닙니다.")
+        return
+    for filename in os.listdir(directory):  
+        if filename.endswith(old_ext):
+            old_file_path=directory+"\\"+filename
+            new_filename=filename.replace(old_ext, new_ext)
+            new_file_path=directory+"\\"+new_filename
+            os.rename(old_file_path, new_file_path)
+            print(f"'{filename}' 파일의 '{new_filename}'(으)로 변경했습니다.")
+
+target_directory="d:\\test"
+old_extension='.c'
+new_extension='.py'
+change_extension(target_directory, old_extension, new_extension)    
---
+++ 10.module/timedelta.py
@@ -0,0 +1,4 @@
+from datetime import datetime, timedelta
+
+current_time=datetime.nonw()
+print("현재 시간:",current_time)    
